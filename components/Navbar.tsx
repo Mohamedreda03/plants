b8d9bbb7c1685c7b944be2c31b53fa70962161d0
Mohamed Reda <mo03.reda@gmail.com>
2024-09-19 12:49:04 +0300

    fsd

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   const t = useTranslations("Navbar");
 
   return (
-    <header className="sticky top-0 inset-x-0 z-[1000] bg-white w-full border-b border-green-200 py-1">
+    <header className="sticky top-0 inset-x-0 z-[50] bg-white w-full border-b border-green-200 py-1">
       <div className="max-w-screen-2xl mx-auto w-full flex items-center justify-between h-[80px] px-7">
         <Link href="/">
           <Image src="/logo.png" alt="Logo" width={100} height={70} />
@@ -55,9 +55,7 @@ export default function Navbar() {
         <div className="hidden md:block">
           <LangButton />
         </div>
-        <div className="z-[2000]">
-          <MobileMenu />
-        </div>
+        <MobileMenu />
       </div>
     </header>
   );
