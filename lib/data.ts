b3a1c86a3d0289d84e39b5635082dacd6350cfcb
Mohamed Reda <mo03.reda@gmail.com>
2024-10-12 22:29:52 +0300

    products edit

--- lib/data.ts
+++ lib/data.ts
@@ -3,10 +3,10 @@ export interface PRODUCTS_TYPE {
   ar_name: string;
   en_name: string;
   la_name: string;
-  height_from?: string;
-  height_to?: string;
-  width_from?: string;
-  width_to?: string;
+  height_from: string;
+  height_to: string;
+  width_from: string;
+  width_to: string;
   ar_suitable_environment: string;
   en_suitable_environment: string;
   category: string;
