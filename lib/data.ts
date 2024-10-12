8ad9c4c5f21c100af4a1fbc103eccfa7692242db
Mohamed Reda <mo03.reda@gmail.com>
2024-10-12 22:12:55 +0300

    products edit

--- lib/data.ts
+++ lib/data.ts
@@ -1,32 +1,14 @@
-export type PRODUCTS_TYPE = {
+export interface PRODUCTS_TYPE {
   id: string;
   ar_name: string;
   en_name: string;
   la_name: string;
-  height_from: string;
-  height_to: string;
-  width_from: string;
-  width_to: string;
+  height_from?: string; // Make optional
+  height_to?: string; // Make optional
+  width_from?: string; // Make optional
+  width_to?: string; // Make optional
   ar_suitable_environment: string;
   en_suitable_environment: string;
-  images: string[];
   category: string;
-};
-
-export const CATEGORY_DATA = [
-  {
-    id: "1",
-    ar_name: "شجيرات",
-    en_name: "Shrubs",
-  },
-  {
-    id: "2",
-    ar_name: "أشجار",
-    en_name: "Trees",
-  },
-  {
-    id: "3",
-    ar_name: "نباتات داخلية",
-    en_name: "Indoor Plants",
-  },
-];
+  images: string[];
+}
