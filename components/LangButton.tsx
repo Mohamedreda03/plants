c2eadbe890d359357e947dd575ba0fa1cb07d222
Mohamed Reda <mo03.reda@gmail.com>
2024-10-28 22:37:34 +0300

    fsf

--- components/LangButton.tsx
+++ components/LangButton.tsx
@@ -16,13 +16,14 @@ export default function LangButton({
     <Link
       href={link ? link : "/"}
       locale={locale === "ar" ? "en" : "ar"}
-      className={cn(className)}
+      className={cn(className, "flex items-center gap-3 border p-2 rounded")}
     >
+      <span>{locale === "ar" ? "English" : "Arabic"}</span>
       <Image
         src={locale === "ar" ? "/en.png" : "/ar.png"}
         className="object-cover"
         height={25}
-        width={40}
+        width={35}
         alt=""
       />
     </Link>
